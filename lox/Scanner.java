4517379f4aca81ba61b4d213474ad6fd901ad456
David Gibson <dgibson314@gmail.com>
2021-09-03 16:11:41 -0400

    new tools and reorg

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -1,11 +1,11 @@
-package com.craftinginterpreters.lox;
+package lox;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.craftinginterpreters.lox.TokenType.*;
+import static lox.TokenType.*;
 
 class Scanner {
     private final String source;
@@ -14,6 +14,28 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",     AND);
+        keywords.put("class",   CLASS);
+        keywords.put("else",    ELSE);
+        keywords.put("false",   FALSE);
+        keywords.put("for",     FOR);
+        keywords.put("fun",     FUN);
+        keywords.put("if",      IF);
+        keywords.put("nil",     NIL);
+        keywords.put("or",      OR);
+        keywords.put("print",   PRINT);
+        keywords.put("return",  RETURN);
+        keywords.put("super",   SUPER);
+        keywords.put("this",    THIS);
+        keywords.put("true",    TRUE);
+        keywords.put("var",     VAR);
+        keywords.put("while",   WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -80,6 +102,9 @@ class Scanner {
                 if (isDigit(c)) {
                     number();
                 }
+                else if (isAlpha(c)) {
+                    identifier();
+                }
                 else {
                     Lox.error(line, "Unexpected character.");
                 }
@@ -87,11 +112,22 @@ class Scanner {
         }
     }
 
+    private void identifier() {
+        // Check if lexeme matches any of the reserved words
+        // in the keywords map. If not it must be an identifier.
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
     private void number() {
         while (isDigit(peek())) advance();
 
         // Look for fractional part
-        if (peek() == '.' && isDigit(peekNext()) {
+        if (peek() == '.' && isDigit(peekNext())) {
             // Consume the "."
             advance();
 
@@ -136,7 +172,17 @@ class Scanner {
 
     private char peekNext() {
         if (current + 1 >= source.length()) return '\0';
-        return source.charAt(current + 1)
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c == '_');
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
@@ -157,7 +203,7 @@ class Scanner {
 
     private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
-        tokens.add(new Token(type, text, literal, linne));
+        tokens.add(new Token(type, text, literal, line));
     }
 
 }
